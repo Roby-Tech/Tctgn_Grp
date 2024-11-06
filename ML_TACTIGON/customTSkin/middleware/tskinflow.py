6115c247b79b88a76859279b734d63e2ced3049f
Roby-Tech <156793006+Roby-Tech@users.noreply.github.com>
2024-11-06 16:50:28 +0100

    Add files via upload

--- ML_TACTIGON/customTSkin/middleware/tskinflow.py
+++ ML_TACTIGON/customTSkin/middleware/tskinflow.py
@@ -2,6 +2,8 @@ from multiprocessing import Process, Event
 from multiprocessing.connection import _ConnectionBase
 import csv
 import time
+import math 
+from math import sqrt 
 
 class TSkinFlow(Process):
     def __init__(self, sensor_rx: _ConnectionBase, csv_file='sensor_data.csv'):
@@ -18,19 +20,34 @@ class TSkinFlow(Process):
         while True:
             if self.sensor_rx.poll(1):  # Attendi fino a 1 secondo per ricevere dati
                 data = self.sensor_rx.recv()
-                print(data)  # Diagnostica: stampa i dati ricevuti
+                # Diagnostica: stampa i dati ricevuti
+
 
                 # Verifica che `data` sia una lista della lunghezza corretta
                 if isinstance(data, list) and len(data) >= 6:
                     # Estrai i valori di accelerazione e giroscopio dalla lista
                     accX, accY, accZ = data[0], data[1], data[2]
+                    accZ = accZ + 9.81
+                    accTOT = sqrt(accX**2 + accY**2 + accZ**2)-9.81
                     gyroX, gyroY, gyroZ = data[3], data[4], data[5]
+                    gyroTOT = sqrt(gyroX**2 + gyroY**2 + gyroZ**2)
                     
                     # Aggiungi una riga al CSV con i dati e un timestamp
                     with open(self.csv_file, mode='a', newline='') as file:
                         writer = csv.writer(file)
                         writer.writerow([time.time(), accX, accY, accZ, gyroX, gyroY, gyroZ])
+                        
                 else:
                     print("Errore: dati ricevuti in un formato non previsto:", data)
 
+                if accTOT < 2:
+                    print('Fermo')
+                else:
+                    print('Movimento rilevato')
+                    #print(accTOT)
+                    #print(data)
+                if gyroTOT < 1:
+                    print('Rotazione ferma')
+                else:
+                    print('Rotazione in corso')
 
